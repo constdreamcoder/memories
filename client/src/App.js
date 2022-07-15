2e237108d17d72ad42148990ba7813596468b7f4
suchan <scdreamricher@gmail.com>
2022-07-15 17:05:17 +0900

    Create a Update feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import { useDispatch } from "react-redux";
@@ -7,15 +8,15 @@ import memories from "./images/memories.png";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from "./styles";
-import { useEffect } from "react";
 
 function App() {
+	const [currentId, setCurrentId] = useState(null);
 	const classes = useStyles();
 	const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(getPosts());
-	}, [dispatch]);
+	}, [currentId, dispatch]);
 
 	return (
 		<Container maxWidth="lg">
@@ -34,10 +35,10 @@ function App() {
 						spacing={3}
 					>
 						<Grid item xs={12} sm={7}>
-							<Posts />
+							<Posts setCurrentId={setCurrentId} />
 						</Grid>
 						<Grid item xs={12} sm={4}>
-							<Form />
+							<Form currentId={currentId} setCurrentId={setCurrentId} />
 						</Grid>
 					</Grid>
 				</Container>
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
+import { useDispatch, useSelector } from "react-redux";
+import { createPost, updatePost } from "../../actions/posts";
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
 	const [postData, setPostData] = useState({
 		creator: "",
 		title: "",
 		message: "",
 		tags: "",
-		selectFile: "",
+		selectedFile: "",
 	});
 	const classes = useStyles();
-	const handleSubmit = () => {};
-	const clear = () => {};
+	const dispatch = useDispatch();
+	const post = useSelector((state) =>
+		currentId ? state.posts.find((p) => p._id === currentId) : null
+	);
+	useEffect(() => {
+		if (post) setPostData(post);
+	}, [post]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (currentId) {
+			dispatch(updatePost(currentId, postData));
+		} else {
+			dispatch(createPost(postData));
+		}
+		clear();
+	};
+	const clear = () => {
+		setCurrentId(null);
+		setPostData({
+			creator: "",
+			title: "",
+			message: "",
+			tags: "",
+			selectedFile: "",
+		});
+	};
+
 	return (
 		<Paper className={classes.paper}>
 			<form
@@ -22,7 +51,9 @@ const Form = () => {
 				className={`${classes.root} ${classes.form}`}
 				onSubmit={handleSubmit}
 			>
-				<Typography variant="h6">Creating a Memory</Typography>
+				<Typography variant="h6">
+					{currentId ? "Editing" : "Creating"} a Memory
+				</Typography>
 				<TextField
 					name="creator"
 					variant="outlined"
@@ -65,7 +96,7 @@ const Form = () => {
 						type="file"
 						multiple={false}
 						onDone={({ base64 }) =>
-							setPostData({ ...postData, selectFile: base64 })
+							setPostData({ ...postData, selectedFile: base64 })
 						}
 					/>
 				</div>
