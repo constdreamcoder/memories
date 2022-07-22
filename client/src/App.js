80fc6884261b99981d00272a4d480eb803a76cbd
suchan <scdreamricher@gmail.com>
2022-07-22 11:59:51 +0900

    Create a pagination feature.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./App.css";
 import { Container } from "@material-ui/core";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 function App() {
+	const user = JSON.parse(localStorage.getItem("profile"));
 	return (
 		<BrowserRouter>
-			<Container maxWidth="lg">
+			<Container maxWidth="xl">
 				<Navbar />
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="auth" element={<Auth />} />
+					<Route path="/" element={<Navigate to="/posts" replace />} />
+					<Route path="/posts" element={<Home />} />
+					<Route path="/posts/search" element={<Home />} />
+					<Route path="/posts/:id" element={<PostDetails />} />
+					<Route
+						path="/auth"
+						element={!user ? <Auth /> : <Navigate to="/posts" replace />}
+					/>
 				</Routes>
 			</Container>
 		</BrowserRouter>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,11 @@
-import { Container, Grid, Grow } from "@material-ui/core";
+import { Container, Grid, Grow, Paper } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import useStyles from "./styles";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
+import Pagination from "../Pagination/Pagination";
 
 const Home = () => {
 	const classes = useStyles();
@@ -30,6 +31,9 @@ const Home = () => {
 					</Grid>
 					<Grid item xs={12} sm={4}>
 						<Form currentId={currentId} setCurrentId={setCurrentId} />
+						<Paper elevation={6}>
+							<Pagination />
+						</Paper>
 					</Grid>
 				</Grid>
 			</Container>
